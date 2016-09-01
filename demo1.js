51b83dbe0b4336dde3dd55a0a019f7785c1d5e37
maggieswan <425172991@qq.com>
2016-09-01 11:15:55 +0800

    branch conflic new1

--- demo1.js
+++ demo1.js
@@ -3,9 +3,12 @@ function (id){
 	var  obj=document.getElementById(id);
 	var  disX=0;
 	var  disY=0;
-	document.onmousedown=function(){
+	obj.onmousedown=function(ev){
 		disX=ev.pageX-obj.offsetLeft;
 		disY=ev.pageY-obj.offsetTop;
-		//
+		document.onmousemove=function(ev){
+			obj.style.left=ev.clientX+disX+'px';
+			obj.style.top=ev.clientY+disY+'px';
+		};
 	}
 }
